8829a72f089a787e16de18ff385a9bf26ad4e375
swati-patill <patil.swati0821@gmail.com>
2021-10-19 19:01:38 +0530

    first

---
+++ DemoClass.java
@@ -0,0 +1,14 @@
+package core.javapractice;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Duggu");
+		int budget = 400;
+		int price = 400;
+		if (budget == price)
+			System.out.println("You can Buy pizza please pay" + price);
+
+	}
+
+}
